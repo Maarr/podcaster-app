c60d5f7e6bbd9abdc86c5321f1f394f780161c62
Mar <marrm9851@gmail.com>
2024-09-13 22:12:18 +0200

    feat: :sparkles: cache cleaning process

--- src/infrastructure/cache/localStorageCache.util.ts
+++ src/infrastructure/cache/localStorageCache.util.ts
@@ -9,13 +9,38 @@ export const getFromCache = <T>(
 
   if (!cachedData || !cachedTimestamp) return null
 
-  const now = new Date().getTime()
-  const lastFetch = new Date(cachedTimestamp).getTime()
-
-  return now - lastFetch < validityTime ? JSON.parse(cachedData) : null
+  return isValidTimestamp(cachedTimestamp, validityTime)
+    ? JSON.parse(cachedData)
+    : null
 }
 
 export const saveToCache = <T>(key: string, data: T): void => {
+  cleanChache()
   localStorage.setItem(key, JSON.stringify(data))
   localStorage.setItem(`${key}_timestamp`, new Date().toISOString())
+}
+
+export const cleanChache = (validityTime = VALIDITY_TIME_MS) => {
+  Object.keys(localStorage)
+    .filter((key) => key.endsWith('_timestamp'))
+    .forEach((key) => {
+      const timestamp = localStorage.getItem(key)
+      if (!timestamp) return
+
+      if (!isValidTimestamp(timestamp, validityTime)) {
+        const itemKey = key.replace('_timestamp', '')
+        localStorage.removeItem(key)
+        localStorage.removeItem(itemKey)
+      }
+    })
+}
+
+const isValidTimestamp = (
+  timestamp: string,
+  validityTime = VALIDITY_TIME_MS
+) => {
+  const now = new Date().getTime()
+  const lastFetch = new Date(timestamp).getTime()
+
+  return now - lastFetch < validityTime
 }
