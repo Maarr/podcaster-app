c019ec9cf2bb6bdee831f8fc242239ec9cb7981c
Mar <marrm9851@gmail.com>
2024-09-14 11:21:20 +0200

    test: config jest and react testing library

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.(ts|tsx|js|jsx)'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+}
