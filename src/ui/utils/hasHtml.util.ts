9a39956011de579415ba1923d66d3986c9fdd898
Mar <marrm9851@gmail.com>
2024-09-19 21:24:01 +0200

    fix: hasHtml util

--- src/ui/utils/hasHtml.util.ts
+++ src/ui/utils/hasHtml.util.ts
@@ -1,4 +1,5 @@
 export function hasHtml(content: string): boolean {
-  const htmlPattern = /<\/?[a-z][\s\S]*>/i
+  const htmlPattern =
+    /<([a-z][a-z0-9]*)\b[^>]*>(.*?)<\/\1>|<([a-z][a-z0-9]*)\b[^>]*\/>/gi
   return htmlPattern.test(content)
 }
